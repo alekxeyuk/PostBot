348359614525cc2bdf97d56edbe081f39436f0fb
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2023-08-29 18:42:46 +0300

    Update __init__.py

--- conf/__init__.py
+++ conf/__init__.py
@@ -5,7 +5,7 @@ REDIS_URL = None # "rediss://:6380/0"
 TELEGRAM_URL = None # "https://api.telegram.org//"
 TELEGRAM_CHANNEL = None # "@"
 
-if not any(REDIS_URL, TELEGRAM_URL, TELEGRAM_CHANNEL):
+if not any([REDIS_URL, TELEGRAM_URL, TELEGRAM_CHANNEL]):
     REDIS_URL = os.environ["REDIS_URL"]
     TELEGRAM_URL = os.environ["TELEGRAM_URL"]
     TELEGRAM_CHANNEL = os.environ["TELEGRAM_CHANNEL"]
